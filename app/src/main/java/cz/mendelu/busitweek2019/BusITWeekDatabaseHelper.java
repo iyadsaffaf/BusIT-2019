f95a4933123874150087b092221ce51f4f9d00ff
shevshinko <45826418+shevshinko@users.noreply.github.com>
2019-02-07 19:35:02 +0100

    beacons added

--- app/src/main/java/cz/mendelu/busitweek2019/BusITWeekDatabaseHelper.java
+++ app/src/main/java/cz/mendelu/busitweek2019/BusITWeekDatabaseHelper.java
@@ -20,8 +20,8 @@ public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
         firstStage(49.209790, 16.615008);
         secondStage(49.210025, 16.614832);
         thirdStage(49.210083, 16.614406);
-        fourthStage(49.212126, 16.617280);
-        fifthStage(49.212126, 16.617280);
+        fourthStage(29028 ,54274);
+        fifthStage(29028 ,54274);
     }
 
     /**
@@ -108,34 +108,34 @@ public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
     /**
      * Register the [fourth] stage of the app.
      *
-     * @param latitude  latitude of the stage.
-     * @param longitude longitude of the stage.
+     * @param major  major of the stage.
+     * @param minor minor of the stage.
      */
-    private void fourthStage(double latitude, double longitude) {
-        taskHelper.addNextStage(latitude, longitude);
+    private void fourthStage(double major, double minor) {
+        taskHelper.addNextStage(major, minor);
 
-        taskHelper.defaultGPSTask(4, 0, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(4, 0, (builder) -> builder.choicePuzzle()
                 .puzzleTime(40000)
                 .question("What is the name of the nearest university bus stop?")
                 .addChoice("Sportovní", false)
                 .addChoice("Erbenova", true)
                 .addChoice("Zimní Stadion", false));
 
-        taskHelper.defaultGPSTask(4, 1, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(4, 1, (builder) -> builder.choicePuzzle()
                 .puzzleTime(40000)
                 .question("At the city tour a legend was told about a creature. Which animal was this?")
                 .addChoice("Crocodile", true)
                 .addChoice("Dragon", false)
                 .addChoice("Whale", false));
 
-        taskHelper.defaultGPSTask(4, 2, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(4, 2, (builder) -> builder.choicePuzzle()
                 .puzzleTime(30000)
                 .question("How old is the Mendel University?")
                 .addChoice("50 years", false)
                 .addChoice("90 years", false)
                 .addChoice("100 years", true));
 
-        taskHelper.defaultGPSTask(4, 3, (builder) -> builder.imageSelectPuzzle()
+        taskHelper.defaultBeaconTask(4, 3, (builder) -> builder.imageSelectPuzzle()
                 .puzzleTime(40000)
                 .question("Who was the most famous player of the Czech Republic?")
                 .addImage(R.drawable.q12_1, false)
@@ -146,27 +146,27 @@ public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
     /**
      * Register the [fifth] stage of the app.
      *
-     * @param latitude  latitude of the stage.
-     * @param longitude longitude of the stage.
+     * @param minor  minor of the stage.
+     * @param major major of the stage.
      */
-    private void fifthStage(double latitude, double longitude) {
-        taskHelper.addNextStage(latitude, longitude);
+    private void fifthStage(double minor, double major) {
+        taskHelper.addNextStage(minor, major);
 
-        taskHelper.defaultGPSTask(5, 0, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(5, 0, (builder) -> builder.choicePuzzle()
                 .puzzleTime(40000)
                 .question("During the city tour, we world told about an old map of the city. Which century was the map made?")
                 .addChoice("19th", false)
                 .addChoice("18th", false)
                 .addChoice("17th", true));
 
-        taskHelper.defaultGPSTask(5, 1, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(5, 1, (builder) -> builder.choicePuzzle()
                 .puzzleTime(40000)
                 .question("What is the most famous sport in Brno?")
                 .addChoice("Ice hockey", true)
                 .addChoice("Tennis", false)
                 .addChoice("Basketball", false));
 
-        taskHelper.defaultGPSTask(5, 2, (builder) -> builder.choicePuzzle()
+        taskHelper.defaultBeaconTask(5, 2, (builder) -> builder.choicePuzzle()
                 .puzzleTime(40000)
                 .question("What is the most popular beer in the Czech Republic?")
                 .addChoice("IPA", false)
