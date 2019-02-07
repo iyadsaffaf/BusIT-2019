1eb47a430f28bfec25460ac2095f50615de354e8
MartijnVisser98 <m.visser.2@hotmail.com>
2019-02-07 11:01:53 +0100

    Added beacon support

--- app/src/main/java/cz/mendelu/busitweek2019/DefaultTaskHelper.java
+++ app/src/main/java/cz/mendelu/busitweek2019/DefaultTaskHelper.java
@@ -3,6 +3,7 @@ package cz.mendelu.busitweek2019;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import cz.mendelu.busItWeek.library.builder.PuzzleBuilder;
@@ -16,10 +17,12 @@ class DefaultTaskHelper {
 
     private final StoryLineBuilder builder;
     private List<LatLng> locations;
+    private HashMap<LatLng, Integer[]> beacons;
 
     DefaultTaskHelper(StoryLineBuilder builder) {
         this.builder = builder;
         locations = new ArrayList<>();
+        beacons = new HashMap<>();
     }
 
     /**
@@ -29,6 +32,44 @@ class DefaultTaskHelper {
      */
     void addNextStage(double latitude, double longitude){
         locations.add(new LatLng(latitude, longitude));
+    }
+
+    /**
+     * Register the next stage in the arraylist.
+     * Register with minor / major
+     *
+     * @param latitude Double representing the Latitude of the stage.
+     * @param longitude Double representing the Longitude of the stage.
+     */
+    void addNextStage(double latitude, double longitude, int major, int minor){
+        LatLng location = new LatLng(latitude, longitude);
+        locations.add(location);
+        beacons.put(location, new Integer[]{major, minor});
+    }
+
+
+
+    /**
+     * Register a basic GPS task.
+     *
+     * @param stage Stage of the task.
+     * @param stepsBack Amount of steps of the task.
+     * @return The builder.
+     */
+    StoryLineBuilder defaultBeaconTask(int stage, int stepsBack, DefaultPuzzleBuilder defaultTaskBuilder){
+        if(stage > locations.size()){
+            throw new NullPointerException(String.format("Requesting stage %d, but only %d stages registered.", stage, stepsBack));
+        }
+
+        TaskBuilder taskBuilder =  builder.addBeaconTask(Integer.toString(stage) + "-" + Integer.toString(stepsBack + 1))
+                .beacon(beacons.get(locations.get(stage - 1))[0], beacons.get(locations.get(stage - 1))[1])
+                .location(locations.get(stage - 1).getLatitude(), locations.get(stage - 1).getLongitude())
+                .victoryPoints(VICTORY_POINTS);
+
+        PuzzleBuilder puzzleBuilder = defaultTaskBuilder.build(taskBuilder);
+        puzzleBuilder = (PuzzleBuilder) puzzleBuilder.puzzleTime(30000);
+
+        return ((TaskBuilder) puzzleBuilder.puzzleDone()).taskDone();
     }
 
     /**
